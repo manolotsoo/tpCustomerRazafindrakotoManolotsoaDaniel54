2bc332c992602225c586505032c98413f3acface
Manolotsoa <manolotsoadaniel@gmail.com>
2024-03-24 16:48:11 +0300

    Ajout table client

---
+++ src/main/java/mg/manolotsoa/tpCustomerRazafindrakotoManolotsoaDaniel54/jsf/CustomerBean.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.manolotsoa.tpCustomerRazafindrakotoManolotsoaDaniel54.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import mg.manolotsoa.tpCustomerRazafindrakotoManolotsoaDaniel54.entity.Customer;
+import mg.manolotsoa.tpCustomerRazafindrakotoManolotsoaDaniel54.service.CustomerManager;
+
+/**
+ * Backing bean pour la page JSF customerList.
+ *
+ * @author manou
+ */
+@Named(value = "customerBean")
+@ViewScoped
+public class CustomerBean implements Serializable {
+
+    private List<Customer> customerList;
+
+    @Inject
+    private CustomerManager customerManager;
+
+    /**
+     * Creates a new instance of CustomerBean
+     */
+    public CustomerBean() {
+    }
+
+    /**
+     * Retourne la liste des clients pour affichage dans une DataTable.
+     */
+    public List<Customer> getCustomers() {
+        if (customerList == null) {
+            customerList = customerManager.getAllCustomers();
+        }
+        return customerList;
+    }
+
+}
